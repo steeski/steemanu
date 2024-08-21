99f2dca687cdd289fb22f25b04db4dcde3dd1e7f
steeski <167318267+steeski@users.noreply.github.com>
2024-08-21 07:43:00 -0300

    aaaaa

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-const
+const caixaPrincipal = document.querySelector ('.caixa-principal');
+const caixaPerguntas = document.querySelector ('.caixa-perguntas');
+const caixaAlternativas = document.querySelector ('.caixa-alternativas');
+const caixaResultado = document.querySelector ('.caixa-resultado');
+const textoResultado = document.querySelector ('.texto-resultado');
+
+const alternativas = [
+    {
+        enunciado: "No âmbito social, você prefere:",
+        alternativa: [
+            {
+                textp: "Alternativa 1",
+                afirmaçao: "Afirmação 1",
+            },
+            {
+                textp: "Alternativa 2",
+                afirmaçao: "Afirmação 2",
+            },
+        ]
+        
+    },
+
+    {
+        enunciado: "No âmbito ambiental, você prefere:",
+        alternativa: [
+            {
+                textp: "Alternativa 3",
+                afirmaçao: "Afirmação 3",
+            },
+            {
+                textp: "Alternativa 4",
+                afirmaçao: "Afirmação 4",
+            },
+        ]
+        
+    },
+
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
+        alternativa: [
+            {
+                textp: "Alternativa 5",
+                afirmaçao: "Afirmação 5",
+            },
+            {
+                textp: "Alternativa 6",
+                afirmaçao: "Afirmação 6",
+            },
+        ]
+        
+    },
+]
+
