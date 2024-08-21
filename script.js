fd3f02c6e9be4332e42a607867819ffb0e942928
steeski <167318267+steeski@users.noreply.github.com>
2024-08-21 08:10:39 -0300

    hg

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function mostrarPerguntas () {
 
 
 function mostrarAlternativas(){
-    for (const opcao of perguntaAtual.alternativas) {
+    for (const opcao of perguntaAtual.alternativa) {
         const botaoAlternativa = document.createElement('button');
         botaoAlternativa.textContent = opcao.texto;
         caixaAlternativas.appendChild(botaoAlternativa);
