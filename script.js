d1ef92713c14cb2e0f8f9972925d8ea3b04cbb75
steeski <167318267+steeski@users.noreply.github.com>
2024-08-28 08:32:21 -0300

    aaaaa

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const alternativas = [
         enunciado: "No âmbito social, você prefere:",
         alternativa: [
             {
-                texto: "Você pode escolher qualquer poder que quiser, porém toda sua família morre",
-                afirmacao: "Parabéns,além de orfão egoista"
+                texto: "Descobrir a cura do câncer, porém você ficará com uma doença incurável e tem pouco tempo de vida",
+                afirmacao: "Parabéns você salvou milhares de pessoas no mundo, e ganhou o premio nobel"
             },
             {
-                texto: "Você só poderá falar com minhoca, porém toda sua família vive",
-                afirmacao: "Tomara que você goste de terra"
+                texto: "Ter todo o dinheiro do mundo com uma empresa multimilionária, mas um parente seu importante tá com uma doença incurável",
+                afirmacao: "Perdeu seu ente que você amava muito, para ficar rico com dinheiro sujo."
             }
         ]
 
@@ -23,12 +23,12 @@ const alternativas = [
         enunciado: "No âmbito ambiental, você prefere:",
         alternativa: [
             {
-                texto: "Ter todo o dinheiro que quiser, mas a Amazônia morre",
-                afirmacao: "Oq adianta não vai ter qualidade de vida?"
+                texto: "Você será o(a)  presidente de um país, porém o país está com nível extremo de poluição no ar e muitas pessoas estão com problema respiratório",
+                afirmacao: "Você virou genocida"
             },
             {
-                texto: "Você pode salvar a Amazônia, mas você nunca mais terá nenhum real na conta",
-                afirmacao: "Vai viver de favor mas a amazonia agradece"
+                texto: "Você será da marinha, porém está tendo guerras marítimas e morrendo muitas pessoas e animais marinhos e sua única opção é se entregar pro inimigo.",
+                afirmacao: "O inimigo não te matou e você virou um herói"
             }
         ]
 
@@ -37,12 +37,12 @@ const alternativas = [
         enunciado: "No âmbito tecnológico, você prefere:",
         alternativa: [
             {
-                texto: "A internet do mundo todo para, mas você consegue inventar algo e se torna a pessoa mais rica da história",
-                afirmacao: "EGOISTAAA"
+                texto: "Você criou uma inteligência artificial para ajudar na polícia, porém esse robô se virou contra o mundo por conta de maus tratos e abuso de poder. Sua única opção é a guerra de humanos x máquinas",
+                afirmacao: "Você vai criar uma IA e uma arma para combater e salvar o mundo e os robos vão se render"
             },
             {
-                texto: "A Nasa te contrata para o cargo mais alto da empresa, mas metade da população morre por conta do seu trabalho",
-                afirmacao: "Os outros não importam neh... ZÉ RUELA"
+                texto: "Você descobriu um vírus da antiguidade por conta das tecnologias avançadas, porém o vírus saiu fora do controle e sua única chance é fugir do planeta sozinho com três desconhecidos.  ",
+                afirmacao: "Você escolheu fugir e um dos desconhecidos era um assassino."
             }
         ]
 
